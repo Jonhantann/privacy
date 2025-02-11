0c6e4c42eddbe67325bc02313ae55e597ca99a8b
Jonhantann <127808337+Jonhantann@users.noreply.github.com>
2025-02-11 19:43:14 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,8 @@
+const images = [
+    "https://images2.imgbox.com/17/47/hw0saJ4H_o.jpg",
+    "https://images2.imgbox.com/b7/a1/BEQcEM5s_o.jpg",
+    "https://images2.imgbox.com/54/9b/3sS0fDQy_o.jpg",
+    // Adicione os outros links aqui...
+];
+
+export default images;
